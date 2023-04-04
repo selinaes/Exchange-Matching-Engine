2fe5dc39213b322564d50329e5e904bb26a34670
FengyiQuan <quan.f@husky.neu.edu>
2023-04-04 17:04:24 -0400

    test

--- src/test/java/TestCreate.java
+++ src/test/java/TestCreate.java
@@ -51,12 +51,108 @@ public class TestCreate {
   }
 
   @Test
-  public void createAll() {
+  public void create6() {
+    UserController client = new UserController();
+    client.startConnection("localhost", 12345);
+    client.sendFile("src/main/java/org/example/create6.txt");
+    client.stopConnection();
+
+  }
+
+  @Test
+  public void create7() {
+    UserController client = new UserController();
+    client.startConnection("localhost", 12345);
+    client.sendFile("src/main/java/org/example/create7.txt");
+    client.stopConnection();
+
+  }
+
+  @Test
+  public void createAll() throws InterruptedException {
+    Thread server = new Thread(() -> Main.main(null));
+    Thread client1 = new Thread(this::create1);
+    Thread client2 = new Thread(this::create2);
+    Thread client3 = new Thread(this::create3);
+    Thread client4 = new Thread(this::create4);
+    Thread client5 = new Thread(this::create5);
+    Thread client6 = new Thread(this::create6);
+    Thread client7 = new Thread(this::create7);
+
+    server.start();
+    client1.start();
+    client2.start();
+    client3.start();
+    client4.start();
+    client5.start();
+    client6.start();
+    client7.start();
+  }
+
+  @Test
+  public void trans1() {
+    UserController client = new UserController();
+    client.startConnection("localhost", 12345);
+    client.sendFile("src/main/java/org/example/transactions1.txt");
+    client.stopConnection();
+  }
+
+  @Test
+  public void trans2() {
+    UserController client = new UserController();
+    client.startConnection("localhost", 12345);
+    client.sendFile("src/main/java/org/example/transactions2.txt");
+    client.stopConnection();
+  }
+
+  @Test
+  public void trans3() {
+    UserController client = new UserController();
+    client.startConnection("localhost", 12345);
+    client.sendFile("src/main/java/org/example/transactions3.txt");
+    client.stopConnection();
+  }
+
+  @Test
+  public void trans4() {
+    UserController client = new UserController();
+    client.startConnection("localhost", 12345);
+    client.sendFile("src/main/java/org/example/transactions4.txt");
+    client.stopConnection();
+  }
+
+  @Test
+  public void trans5() {
+    UserController client = new UserController();
+    client.startConnection("localhost", 12345);
+    client.sendFile("src/main/java/org/example/transactions5.txt");
+    client.stopConnection();
+  }
+
+  @Test
+  public void trans6() {
+    UserController client = new UserController();
+    client.startConnection("localhost", 12345);
+    client.sendFile("src/main/java/org/example/transactions6.txt");
+    client.stopConnection();
+  }
+
+  @Test
+  public void trans7() {
+    UserController client = new UserController();
+    client.startConnection("localhost", 12345);
+    client.sendFile("src/main/java/org/example/transactions7.txt");
+    client.stopConnection();
+  }
+
+  @Test
+  public void transAll() {
     Main.main(null);
-    create1();
-    create2();
-    create3();
-    create4();
-    create5();
+    trans1();
+//    trans2();
+//    trans3();
+//    trans4();
+//    trans5();
+//    trans6();
   }
 }
