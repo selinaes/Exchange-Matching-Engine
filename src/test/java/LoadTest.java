a809dce2ceb710e475a2e12f88496b7984b6e14b
jl1188 <jl1188@duke.edu>
2023-04-08 22:49:44 -0400

    loadtest

--- src/test/java/LoadTest.java
+++ src/test/java/LoadTest.java
@@ -73,8 +73,58 @@ public class LoadTest {
 //    client.sendMessage(sb.toString());
   }
 
+
   @Test
-  public void test() {
+  public void testLoad() {
+    int numRounds = 1;
+    int numRequests = 0;
+    UserController client = new UserController();
+    List<Long> latencies = new ArrayList<>();
+
+    List<List<SubTransactionRequest>> subTrans = testMultiTrans();
+
+    client.startConnection("localhost", 12345);
+    long startTime = System.nanoTime();
+    long requestStartTime;
+    long requestEndTime;
+    long latencyMs;
+    for (int i = 0; i < numRounds; i++) {
+      requestStartTime = System.nanoTime();
+      testCreate();
+      numRequests++;
+      client.recvMsg();
+      requestEndTime = System.nanoTime();
+      latencyMs = (requestEndTime - requestStartTime) / 1000000;
+      latencies.add(latencyMs);
+
+      requestStartTime = System.nanoTime();
+      testTrans();
+      numRequests++;
+      client.recvMsg();
+      requestEndTime = System.nanoTime();
+      latencyMs = (requestEndTime - requestStartTime) / 1000000;
+      latencies.add(latencyMs);
+
+      for (int j = 1; j < 11; j++) {
+          requestStartTime = System.nanoTime();
+          sendTransaction(String.valueOf(j), subTrans.get(j-1));
+          numRequests++;
+        client.recvMsg();
+        requestEndTime = System.nanoTime();
+        latencyMs = (requestEndTime - requestStartTime) / 1000000;
+        latencies.add(latencyMs);
+      }
+    }
+    long endTime = System.nanoTime();
+    long durationMs = (endTime - startTime) / 1000000;
+    client.stopConnection();
+    double throughput = numRequests / (durationMs / 1000.0);
+    double meanLatency = latencies.stream().mapToLong(Long::longValue).average().orElse(0);
+    System.out.println("Sent " + numRequests + " requests in " + durationMs + "ms, throughput: " + throughput + " requests/s, mean latency: " + meanLatency + " ms");
+  }
+
+  @Test
+  public void testCreate() {
     Map<String, Double> create = Map.of("1", 1000.0, "2", 2000.0, "3", 3000.0, "4", 4000.0, "5", 5000.0, "6", 6000.0, "7", 7000.0, "8", 8000.0, "9", 9000.0, "10", 10000.0);
     Map<String, Map<String, Double>> positions = Map.of("A", Map.of("1", 100.0, "2", 200.0, "3", 300.0, "4", 400.0, "5", 500.0, "6", 600.0, "7", 700.0, "8", 800.0, "9", 900.0, "10", 1000.0),
             "B", Map.of("1", 100.0, "2", 200.0, "3", 300.0, "4", 400.0, "5", 500.0, "6", 600.0, "7", 700.0, "8", 800.0, "9", 900.0, "10", 1000.0),
@@ -87,9 +137,10 @@ public class LoadTest {
 
 
 
-  @Test
-  public void testMultiTrans() {
+
+  private List<List<SubTransactionRequest>> testMultiTrans() {
 // Generate 10 sub-transactions
+    List<List<SubTransactionRequest>> subTransactionRequests = new ArrayList<>();
     for (int i = 1; i < 11; i++) {
       List<SubTransactionRequest> orders = new ArrayList<>();
 
@@ -109,7 +160,7 @@ public class LoadTest {
             symbol = "AMD";
             break;
           case 2:
-            symbol = "AAPL";
+            symbol = "APPL";
             break;
         }
         amount = (int) (Math.random() * 100) - 50;
@@ -117,18 +168,18 @@ public class LoadTest {
 
         orders.add(new OrderRequest(Integer.toString(j), symbol, amount, price));
       }
+      subTransactionRequests.add(orders);
       sendTransaction(String.valueOf(i), orders);
 
     }
+    return subTransactionRequests;
   }
 
 
   @Test
   public void testMultipleCreate() {
-//    int i = 0;
-//    System.out.println((char) (0 + 'a'));
     int numIteration = 100;
-    for (int i = 0; i < numIteration; i++) {
+    for (int i = 1; i < numIteration; i++) {
       sendCreateRequest(Map.of(String.valueOf(i),
               (double) i * 1000), Map.of("APPL", Map.of(String.valueOf(i),
               (double) i * 1000), "BTC", Map.of(String.valueOf(i),
@@ -140,7 +191,7 @@ public class LoadTest {
   @Test
   public void testTrans() {
     List<SubTransactionRequest> subTransactionRequests =
-            List.of(new OrderRequest("1", "A", 100, 10.0), new OrderRequest("2", "A", 100, 10.0), new OrderRequest("3", "A", 100, 10.0), new OrderRequest("4", "A", 100, 10.0), new OrderRequest("5", "A", 100, 10.0), new OrderRequest("6", "A", 100, 10.0), new OrderRequest("7", "A", 100, 10.0), new OrderRequest("8", "A", 100, 10.0), new OrderRequest("9", "A", 100, 10.0), new OrderRequest("10", "A", 100, 10.0));
+            List.of(new OrderRequest("1", "BTC", 100, 11.0), new OrderRequest("2", "APPL", 30, 12.0), new OrderRequest("3", "AMD", -20, 14.0), new OrderRequest("4", "APPL", -50, 17.0), new OrderRequest("5", "BTC", 40, 10.0), new OrderRequest("6", "AMD", 30, 16.0), new OrderRequest("7", "AMD", -60, 18.0), new OrderRequest("8", "BTC", -25, 15.0), new OrderRequest("9", "AMD", 40, 14.0), new OrderRequest("10", "APPL", -100, 15.0));
     sendTransaction("1", subTransactionRequests);
   }
 }
